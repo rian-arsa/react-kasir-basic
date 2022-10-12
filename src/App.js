380075ee847fb7fe0e9c409479152482e49a0b51
Rian Ari Sasono <rianono48@gmail.com>
2022-10-12 18:52:00 +0700

    Menambahkan component untuk Navbar, Hasil, dan List Categoies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Hasil, ListCategories, NavbarComponent } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Hasil.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+function Hasil() {
+  return (
+    <Col md={3} mt={2}>
+      <h4>
+        <strong>Daftar Kategori</strong>
+      </h4>
+      <hr />
+    </Col>
+  );
+}
+
+export default Hasil;
